abbce7e525be9aefd4a2f378da3336e10378ab29
Benjamin Peter <benjaminpeter@arcor.de>
2013-03-25 19:25:31 +0100

    Moved up to clojure 1.5

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject clj-pls "0.2.0"
+(defproject clj-pls "0.2.1"
   :description "Small clojure wrapper using ini4j to parse and write .pls playlist files."
   :url "https://github.com/dedeibel/clj-pls"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [
-                 [org.clojure/clojure "1.4.0"]
+                 [org.clojure/clojure "1.5.0"]
                  [org.ini4j/ini4j "0.5.2"]
                  ]
   :repositories [["mavenrepository" "http://repo1.maven.org/maven2/"]]
